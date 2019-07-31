043f9ca1ad963fdcde7292ff4cd53b1542b0d90b
Brady Douthit <brady.douthit99@gmail.com>
2019-07-31 12:18:16 -0400

    wall collision now works, dots will now be randomly placed.

--- logic.js
+++ logic.js
@@ -1,59 +1,64 @@
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '"' + '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  };
+  color += '"'
+  console.log("Color code: " + color)
+};
+function setRandomColor() {
+  
+};
 window.onload = function () {
   Crafty.init(501, 501, document.getElementById('cr-stage'));
-  var randomX = Math.floor(Math.random() * 451);
-  var randomY = Math.floor(Math.random() * 451);
   var playerStats = { x: 10, y: 10, w: 50, h: 50 }
 
 
-  var playerBox = Crafty.e("2D, DOM, Color, Fourway, Collision, Gravity, Player")
-  .attr(playerStats)
-  .collision([0, 16, 16, 0, 32, 16, 16, 32])
-  .color("white")
-  .fourway(2)
-  .bind("Move", function(evt) { // after player moved
-    var hitDatas, hitData;
-    if ((hitDatas = this.hit('Wall'))) { // check for collision with walls
-      hitData = hitDatas[0]; // resolving collision for just one collider
-      if (hitData.type === 'SAT') { // SAT, advanced collision resolution
-        // move player back by amount of overlap
-        this.x -= hitData.overlap * hitData.nx;
-        this.y -= hitData.overlap * hitData.ny;
-      } else { // MBR, simple collision resolution
-        // move player to previous position
-        this.x = evt._x;
-        this.y = evt._y;
+  var playerBox = Crafty.e("2D, DOM, Color, Fourway, Collision, Player")
+    .attr(playerStats)
+    .collision([0, 16, 16, 0, 32, 16, 16, 32])
+    .color("white")
+    .fourway(200)
+    .bind("Moved", function (evt) { // after player moved    
+      if (this.hit('solid')) {
+        this[evt.axis] = evt.oldValue;
       }
-    }
-  });
-  playerBox.z = 2;
+    });
 
-  
-  
-  var wallBottom = Crafty.e('Wall, 2D, DOM, Color, Collision')
+
+
+  var wallBottom = Crafty.e('solid, 2D, DOM, Color, bottom')
     .attr({ x: 0, y: 500, w: 501, h: 1 })
     .color('red');
-  var wallTop = Crafty.e('Wall, 2D, DOM, Color, Collision')
+  var wallTop = Crafty.e('solid, 2D, DOM, Color, top')
     .attr({ x: 0, y: 0, w: 501, h: 1 })
     .color('red');
-  var wallLeft = Crafty.e('Wall, 2D, DOM, Color, Collision')
+  var wallLeft = Crafty.e('solid, 2D, DOM, Color, left')
     .attr({ x: 0, y: 0, w: 1, h: 501 })
     .color('red');
-  var wallRight = Crafty.e('Wall, 2D, DOM, Color, Collision')
+  var wallRight = Crafty.e('solid, 2D, DOM, Color, right')
     .attr({ x: 500, y: 0, w: 1, h: 501 })
     .color('red');
 
 
-  var Dot = function (x, y) {
-    this.x = x;
-    this.y = y
-  }
+
   var randomCoordinates = function () {
-    //for (var i = 0; 1 < 100; i++) {
-    //  var randomX = Math.floor(Math.random() * 3) + 1;
-    //console.log(randomX)
-    //}
+    var randomX = Math.floor(Math.random() * 501);
+    var randomY = Math.floor(Math.random() * 501);
+    Crafty.e("2D, DOM, Color, Fourway, Collision")
+      .attr({ x: randomX, y: randomY, w: 2, h: 2 })
+      .collision([0, 16, 16, 0, 32, 16, 16, 32])
+      .color("white")
+      .fourway(200)
+      .bind("Moved", function (evt) { // after player moved    
+        if (this.hit('solid')) {
+          this[evt.axis] = evt.oldValue;
+        }
+      });
+    getRandomColor();
     console.log(randomX)
     console.log(randomY)
   };
-  $("#start-button").click(randomCoordinates);
+  $("#start-button").click(randomCoordinates)
 }
