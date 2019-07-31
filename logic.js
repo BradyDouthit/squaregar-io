ed0fb0ac06c5c8dc1a8b0a7a466058b0e289b5fd
Brady Douthit <brady.douthit99@gmail.com>
2019-07-31 14:27:04 -0400

    fixed the hitboxes of both the player and the dots

--- logic.js
+++ logic.js
@@ -4,19 +4,21 @@ var randomDotAttributes = function () {
   var dotColor = '#';
   for (var i = 0; i < 6; i++) {
     dotColor += letters[Math.floor(Math.random() * 16)];
-  };
+  };//picks random hex codes for color
 
   var randomX = Math.floor(Math.random() * 501);
-  var randomY = Math.floor(Math.random() * 501);
-  var newDot = Crafty.e("2D, DOM, Color, Collision")
+  var randomY = Math.floor(Math.random() * 501);//sets random x and y values for each dot
+  var newDot = Crafty.e("2D, DOM, Color, Collision, newDot, DebugRectangle")
     .attr({ x: randomX, y: randomY, w: 10, h: 10 })
     .color(dotColor)
-    .collision([0, 16, 16, 0, 32, 16, 16, 32])
-    .bind("Moved", function (evt) { // after player moved    
-      if (this.hit('solid')) {
-        this[evt.axis] = evt.oldValue;
-      }
+    .collision()
+    .debugStroke("red")
+    newDot.checkHits("Player").bind("HitOn", function(hitData) {
+      this.color("green");
+      this.w = this.w + 3;
+      this.h = this.h + 3;
     });
+    newDot.debugRectangle(newDot)
 };
 
 window.onload = function () {
@@ -24,9 +26,11 @@ window.onload = function () {
   var playerStats = { x: 10, y: 10, w: 50, h: 50 }
 
 
-  var playerBox = Crafty.e("2D, DOM, Color, Fourway, Collision, Player")
+  //sets the player entity attributes
+  var playerBox = Crafty.e("2D, DOM, Color, Fourway, Collision, Player, DebugRectangle")
     .attr(playerStats)
-    .collision([0, 16, 16, 0, 32, 16, 16, 32])
+    .collision()
+    .debugStroke('red')
     .color("white")
     .fourway(200)
     .bind("Moved", function (evt) { // after player moved    
@@ -34,9 +38,17 @@ window.onload = function () {
         this[evt.axis] = evt.oldValue;
       }
     });
+    playerBox.debugRectangle(playerBox)
 
+    playerBox.z = 2;
 
+    playerBox.checkHits("newDot").bind("HitOn", function(hitData) {
+      this.color("green");
+      this.w = this.w + 3;
+      this.h = this.h + 3;
+    });
 
+  //sets up walls for collision with the player entity
   var wallBottom = Crafty.e('solid, 2D, DOM, Color, bottom')
     .attr({ x: 0, y: 500, w: 501, h: 1 })
     .color('red');
@@ -50,12 +62,15 @@ window.onload = function () {
     .attr({ x: 500, y: 0, w: 1, h: 501 })
     .color('red');
 }
+
+
+
 $("#start-button").click(function () {
   var countTime = 0;
-  var countIntervalSteps = setInterval(function () {
+  var countIntervalSteps = setInterval(function () {//sets up an interval that stops at 50
     countTime++
     console.log(countTime)
-    randomDotAttributes();
+    randomDotAttributes();//creates a new "dot" on the screen every time this is called
     if (countTime === 50) {
       clearInterval(countIntervalSteps)
     }
